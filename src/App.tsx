5fa1d2bc50c09272a6c3595cc9fbd53708c9646b
Paranietharan <142080100+paranietharan@users.noreply.github.com>
2025-07-22 20:51:19 +0530

    JSON formatter implementation | 404 not found page impl

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/HomePage';
+import JsonFormatterPage from './Pages/JSONFormatterPage';
+import NotFound from './Pages/NotFound';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path='/json-formatter' element={<JsonFormatterPage />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,5 +1,5 @@
-import styles from './Home.module.css';
 import { Link } from 'react-router-dom';
+import styles from './Home.module.css';
 
 const Home = () => (
   <div className={styles.container}>
